69ec69879d795a33f9bd4f9c18f8df67a396c3b1
nappdaddy2000 <mnapper3@ivytech.edu>
2016-12-22 04:11:00 +0000

    added devise

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,22 @@ class ItemsController < ApplicationController
     def show
         @item = Item.find(params[:id])
     end
+    
+    def edit
+    end
+    
+    def update
+        if @item.update(item_params)
+            redirect_to item_path(@item)
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @item.destroy
+        redirect_to root_path
+    end
  
  private
  
